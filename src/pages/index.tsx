f8828c25cc37781f5b60106f423c6aec894eab8b
kianafirozian <kianafirozian96@gmail.com>
2023-08-22 19:01:56 +0330

    Updating the homePage UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import styles from "../styles/Home.module.css";
 import Navbar from "../../comps/Navbar";
 import Footer from "../../comps/footer";
 import Link from "next/link";
@@ -7,21 +8,23 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>HomePage</h1>
-      <p>
+      <h1 className={styles.title}>HomePage</h1>
+      <p className={styles.text}>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur minus
         reprehenderit amet vitae delectus illum, qui magnam. Assumenda atque,
         eligendi totam et consectetur provident minima deserunt. Accusantium
         voluptates fugit dolore.
       </p>
 
-      <p>
+      <p className={styles.text}>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur minus
         reprehenderit amet vitae delectus illum, qui magnam. Assumenda atque,
         eligendi totam et consectetur provident minima deserunt. Accusantium
         voluptates fugit dolore.
       </p>
-      <Link href="/ninjas">See Ninja Listing </Link>
+      <Link href="/ninjas" className={styles.btn}>
+        See Ninja Listing{" "}
+      </Link>
     </div>
   );
 }
