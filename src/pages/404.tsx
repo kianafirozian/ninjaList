f9a2fcf8874ab5fed727c363451e2a62877fa588
kianafirozian <kianafirozian96@gmail.com>
2023-08-23 01:09:49 +0330

    Redirecting back to Home page from 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,17 @@
 import Link from "next/link";
+import { useEffect } from "react";
+// in order to redirect to the home page after we getting error
+import { useRouter } from "next/router";
+
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+      //   redirecting to the home page,we can also use router.go(1)
+    }, 3000);
+  }, []);
   return (
     <div className="not-found">
       <h1>Oooopss...</h1>
